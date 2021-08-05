650e025492efee6fe95f7f1b7d7176fa0ddf287c
gcmastra <gcmastra@yahoo.com>
2021-08-05 17:01:04 -0400

    uploading sqo queries saved through section 7.3.5 and starter code for challenge

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ CREATE TABLE departments (
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
-	frst_name VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
@@ -35,18 +35,7 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
--- add two more table definitions salaries, titles, and dept_emp
-
-SELECT count(*) from employees
--- 300024 employees thats three hundred thousand and some odd employees 
-
-
-SELECT frst_name, last_name FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
-
---- we have a spelling mistake - when I created the employees table
---- I called the column frst_name and left out the i 
---- I may have to recreate the table which will be a pain in the butt 
+-- add two more table definitions - titles, and dept_emp
 
 
 SELECT count(*) FROM employees
@@ -54,7 +43,239 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 --- result 41380
 
+--- this is the last comment of the night 8-3
+
+--- below this are queries from Wed 8-4
+
+--- be careful about the order of the columns as well as the spelling of the labels
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
 
 
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
+);
 
---- this is the last comment of the night 
+select * from departments
+
+DROP TABLE employees CASCADE;
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+select count(*) from employees
+
+select count(*) from salaries
+--- result 0 because it has not been imported yet? I thought I did import it 
+--- after import it has size 300024 just like employees because every employee has a salary 
+
+select count(*) from dept_manager
+=== result 24
+
+select * from dept_manager
+-- note that every manager has an employee ID but the constraint has been removed 
+--- after I dropped the employee table with the CASCADE option
+
+--- the remaining table to create are titles and dept_emp
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+--- import completed 
+
+
+Select count(*) from dept_emp;
+--- result 331603 which is higher than employees because some employees have been in more than one dept
+
+--- give this a try - I am not sure if emp_no should be the PK but I think it is the only one with uniqueness
+-- after the columns are created can the order be changed? Or do I need to create the table again ? 
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+select count(*) from titles 
+-- result 443308 even more than employees because each employee gets a new title when they are promoted
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+-- this version should create a new table to hold the results 
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Day Thu 8.5 section 7.3.3 Joins
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments as d 
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+    FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+     FROM departments as d 
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+=== section 7.3.4 begins here 
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+--- list was not sorted so I added ORDER BY
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+    INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--- section 7.3.5
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+    -- INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    AND (de.to_date = '9999-01-01');
+
+--- question - why does this use de.to_date instead of s.to_date or e.to_date?
+--- if it is a current employee wouldn't their salary dates also be in the future?
+--- maybe I should write a query to answer that question -
+SELECT * from salaries as s
+WHERE (s.to_date = '9999-01-01');
+--- the salary dates are by year not in the future
+--- what if we used the to_date from employee table would that change the results?  
+--- aha I see why- e.to_date is not selected in the query, only the de.to_date is in the SELECT statement
+
+-- Query 2 from this section runs but only returns a short list which is what the course expects
+-- maybe it is because of the order of the two inner joins - or maybe one needs to be a outer join ?
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- query 3 from section 7.3.5
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- is it true that the same employee may have more than one department over time? 
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+ORDER BY ce.emp_no;
+
+
+-- yes I sorted the list on emp_no and we see Kazihide has two records and I am sure there are more 
+-- Jungsoon & Reuven are the next two who have been in more than one dept so we need to know which is the current dept
+-- turns out the course material anticipated this also (after I thoought of it) and these will be in the next section 
+ 
