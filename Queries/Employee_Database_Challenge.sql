07534b9ed529c092f1f2fbd8fd9899fed7e10f0c
gcmastra <gcmastra@yahoo.com>
2021-08-15 15:44:51 -0400

    final query for deliverable two of the challenge

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -54,4 +54,27 @@ FROM _______
 ORDER BY _____, _____ DESC;
 
 --- pause here on Sat 8.14 in order to push files so far for deliverable one
---- resume here with deliverable two on Sunday
+
+-- Deliverable Two - Employees eligible for mentoring program
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+    e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, de.to_date DESC;
+
+-- comments - not sure why they would limit mentoring to people born in only one year? 
+-- someone born in 1965 is 56 years old now - that seems old to be mentored by another person 
+-- count 1549 employees eligible for mentoring? or eligible to be mentors after the old people leave?
